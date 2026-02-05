3973a125bfec59f4d1ccc40eb863e73c90182f04
frhack <frapas@gmail.com>
2026-02-05 10:01:06 +0100

    added

---
+++ ordinamento_selezione_consegna.cpp
@@ -0,0 +1,33 @@
+/*
+ * Questo programma ordina un array di numeri interi con l'algoritmo di selezione
+ * usare le seguenti funzioni: chiediNumeri, stampaNumeri, scambiaNumeri, indiceMinimo (da modificare)
+ */
+
+
+#include <iostream>
+using namespace std;
+
+
+int main() {
+    int arr[10];
+    const int n = 10;
+
+    cout << "Questo programma ordina un array di numeri interi con l'algoritmo di selezione" << endl << endl;
+
+    //chiediNumeri(arr,n);
+
+    // Stampa dell'array
+    cout << endl << "Array inserito: ";
+    
+    //stampaNumeri(arr,n);
+
+    //ordina l'array inserire qui il codice
+
+
+    // Stampa dell'array dopo lo scambio
+    cout << endl << "Array ordinato: ";
+    //stampaNumeri(arr,n);
+    cout << endl;
+
+    return 0;
+}
